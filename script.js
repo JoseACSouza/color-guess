57175a6b2fdf825e54e67f6b25a1b5e8e7a028fb
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-01-04 09:53:29 -0300

    começando a implementar a função de click aos circulos

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 const color = document.getElementsByTagName('section')[0];
 const options = document.getElementsByTagName('section')[1];
 
-//Cor a ser adivinhada
-const answer = document.createElement('p');
-answer.id = 'rgb-color';
-color.appendChild(answer);
+//Criando a tag da cor a ser adivinhada
+const colorAnswer = document.createElement('p');
+colorAnswer.id = 'rgb-color';
+color.appendChild(colorAnswer);
 
 //Criando as opções
 for (let index = 0; index < 6; index+=1){
@@ -18,4 +18,22 @@ for (let index = 0; index < 6; index+=1){
 const itemArray = document.getElementsByClassName('ball');
 for (let index = 0; index < itemArray.length ; index+=1){
     itemArray[index].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+}
+
+//Cor a ser advinhada
+colorAnswer.innerHTML = itemArray[Math.floor(Math.random() * 5)].style.backgroundColor; 
+
+
+//Função aos círculos
+const answer = document.createElement('p');
+answer.id = 'answer';
+answer.innerHTML = 'Escolha uma cor';
+color.appendChild(answer);
+
+for (let index = 0; index < itemArray.length ; index+=1){
+    itemArray[index].addEventListener('click',(target)=>{
+        if (target.style.backgroundColor === colorAnswer.innerHTML){
+            answer.innerHTML = 'Acertou!';
+        } else {answer.innerHTML = "Errou! Tente novamente!"}
+    })
 }
